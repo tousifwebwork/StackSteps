77d991a73c77a678ad4d145ac76149b410271bf1
Mohammad Tousif <tousifwebwork@gmail.com>
2026-02-14 14:35:28 +0530

    Hero image update

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -10,7 +10,7 @@ import { Eye, EyeClosed } from 'lucide-react';
 import useAuthStore from '../store/authStore';
 
 // Hero background image
-const HERO_IMAGE = '../../../public/authbackground.avif';
+const HERO_IMAGE = '/authbackground.avif'; 
 
 // ============================================
 // Login Component
